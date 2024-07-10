4c537f5144ea48017ad4d85115e1ca2f68358e8a
TromiUss <121058447+TromiUss@users.noreply.github.com>
2024-07-10 13:45:04 +0300

    task8

--- js/template-photos.js
+++ js/template-photos.js
@@ -1,30 +1,26 @@
+
 const picturesList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content;
 
-
-const renderTemplate = ({ url, comments, likes }) => {
+const renderTemplate = ({ url, comments, likes, id }) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   const pictureImg = pictureElement.querySelector('.picture__img');
   pictureImg.src = url;
-  const pictureComments = pictureElement.querySelector('.picture__comments');
-  pictureComments.textContent = comments.length;
-  const pictureLikes = pictureElement.querySelector('.picture__likes');
-  pictureLikes.textContent = likes;
-
+  pictureImg.alt = url;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  const pictureContainer = pictureElement.querySelector('.picture');
+  pictureContainer.dataset.id = id;
   return pictureElement;
 };
 
-
 const renderPictures = (photos) => {
   const pictureListFragment = document.createDocumentFragment();
-
   photos.forEach((photo) => {
     const pictureCard = renderTemplate(photo);
     pictureListFragment.appendChild(pictureCard);
   });
-
   picturesList.appendChild(pictureListFragment);
 };
 
-
-export { renderPictures };
+export { renderPictures, picturesList };
