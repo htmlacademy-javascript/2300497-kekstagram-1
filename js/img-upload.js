eafb4ac9e3f7031fa1bd8bdba4ea6803d6b513e7
Nikita <thepeaceofmind@yandex.ru>
2024-07-30 15:55:04 +0300

    lint 1

--- js/img-upload.js
+++ js/img-upload.js
@@ -1,4 +1,4 @@
-import { isEscapeKey } from "./keyboard-keys.js";
+import { isEscapeKey } from './keyboard-keys.js';
 
 const fileInput = document.querySelector('#upload-file');
 const previewImg = document.querySelector('.default-preview-img');
@@ -18,9 +18,25 @@ const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const MAX_HASHTAGS = 5;
 
+// Функция для закрытия оверлея
+const closeOverlay = function() {
+  overlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  fileInput.value = '';
+  previewImg.src = 'img/upload-default-image.jpg';
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const onDocumentKeydown = function(evt) {
+  if (isEscapeKey(evt)) {
+    if (document.activeElement !== hashtagsField && document.activeElement !== descriptionField) {
+      evt.preventDefault();
+      closeOverlay();
+    }
+  }
+};
 
 // Функция для загрузки файла и установки его в preview
-
 const fileLoader = function() {
   const file = fileInput.files[0];
   if (file) {
@@ -36,36 +52,17 @@ const fileLoader = function() {
   }
 };
 
-// Функция для закрытия оверлея
-const closeOverlay = function() {
-  overlay.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  fileInput.value = '';
-  previewImg.src = 'img/upload-default-image.jpg';
-  document.removeEventListener('keydown', onDocumentKeydown);
-};
-
 fileInput.addEventListener('change', fileLoader);
 closeButton.addEventListener('click', closeOverlay);
 
-const onDocumentKeydown = function (evt) {
-  if (isEscapeKey(evt)) {
-    if (document.activeElement !== hashtagsField && document.activeElement !== descriptionField) {
-      evt.preventDefault();
-      closeOverlay();
-    }
-  }
-};
-
-//функция увеличения/уменьшения изображения
-
+// Функция для увеличения/уменьшения изображения
 function updateScaleValue(value) {
   scaleValue.value = `${value}%`;
   const scale = value / 100;
   previewImg.style.transform = `scale(${scale})`;
 }
 
-function bigScale (){
+function bigScale() {
   let currentScale = parseInt(scaleValue.value, 10);
   if (currentScale < MAX_SCALE) {
     currentScale += SCALE_STEP;
@@ -73,7 +70,7 @@ function bigScale (){
   }
 }
 
-function smallScale (){
+function smallScale() {
   let currentScale = parseInt(scaleValue.value, 10);
   if (currentScale > MIN_SCALE) {
     currentScale -= SCALE_STEP;
@@ -84,8 +81,7 @@ function smallScale (){
 scaleBigger.addEventListener('click', bigScale);
 scaleSmaller.addEventListener('click', smallScale);
 
-//реализация фильтров
-
+// Реализация фильтров
 effectsContainer.addEventListener('change', (evt) => {
   if (evt.target.classList.contains('effects__radio')) {
     imgPreview.className = 'default-preview-img';
@@ -95,8 +91,7 @@ effectsContainer.addEventListener('change', (evt) => {
   }
 });
 
-// Реализация валидации через пристин
-
+// Реализация валидации через Pristine
 const pristine = new Pristine(form, {
   classTo: 'form__item',
   errorClass: 'form__item--invalid',
@@ -107,7 +102,9 @@ const pristine = new Pristine(form, {
 });
 
 function validateHashTag(value) {
-  if (value === '') {return true;}
+  if (value === '') {
+    return true;
+  }
 
   const hashtagPattern = /^#[a-zа-яё0-9]{1,19}$/i;
   const hashtags = value.trim().split(/\s+/);
@@ -115,12 +112,11 @@ function validateHashTag(value) {
   if (hashtags.length > MAX_HASHTAGS) {
     return false;
   }
-  return hashtags.every(tag => hashtagPattern.test(tag));
+  return hashtags.every((tag) => hashtagPattern.test(tag));
 }
 
-function validateText (value) {
+function validateText(value) {
   return value.length <= 140;
-
 }
 
 pristine.addValidator(hashtagsField, validateHashTag, 'неверно введенный хэш-тег');
@@ -130,19 +126,22 @@ const validateField = (field) => {
   const isValid = pristine.validate(field);
   field.style.borderColor = isValid ? '' : 'red';
 };
+
+function updateFieldBorder(field) {
+  validateField(field);
+}
+
 function onHashtagsFieldInput() {
   updateFieldBorder(hashtagsField);
 }
 
 hashtagsField.addEventListener('input', onHashtagsFieldInput);
-hashtagsField.removeEventListener('input', onHashtagsFieldInput);
 
 function onDescriptionFieldInput() {
-  updateFieldBorder(descriptionField)
+  updateFieldBorder(descriptionField);
 }
 
 descriptionField.addEventListener('input', onDescriptionFieldInput);
-descriptionField.removeEventListener('input', onDescriptionFieldInput);
 
 form.addEventListener('submit', (evt) => {
   const valid = pristine.validate();
@@ -151,4 +150,4 @@ form.addEventListener('submit', (evt) => {
   }
 });
 
-export {fileLoader, fileInput};
+export { fileLoader, fileInput };
