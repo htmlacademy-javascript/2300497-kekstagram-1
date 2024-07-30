81c384aee88258ff03e1a36210cdbe55f5593f62
Nikita <thepeaceofmind@yandex.ru>
2024-07-30 16:02:57 +0300

    4

--- js/keyboard-keys.js
+++ js/keyboard-keys.js
@@ -1,10 +1,9 @@
 const isEscapeKey = function(evt) {
     return evt.key === 'Escape';
-  };
-  
-  const isEnterKey = function(evt) {
+};
+
+const isEnterKey = function(evt) {
     return evt.key === 'Enter';
-  };
-  
-  export { isEscapeKey, isEnterKey };
-  
+};
+
+export { isEscapeKey, isEnterKey };
