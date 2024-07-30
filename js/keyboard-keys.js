982f29812b07d381f0ed81e090c8cfca8a968fd8
Nikita <thepeaceofmind@yandex.ru>
2024-07-30 15:44:07 +0300

    "used before"

--- js/keyboard-keys.js
+++ js/keyboard-keys.js
@@ -1,6 +1,11 @@
-const isEscapeKey = (evt) => evt.key === 'Escape';
-const isEnterKey = (evt) => evt.key === 'Enter';
-
+const isEscapeKey = function(evt) {
+    return evt.key === 'Escape';
+  };
+  
+  const isEnterKey = function(evt) {
+    return evt.key === 'Enter';
+  };
+  
 
 export {isEscapeKey, isEnterKey};
 
